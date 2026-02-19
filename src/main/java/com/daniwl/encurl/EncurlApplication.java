e99a336041f0a6b7b82010bd0e6057346c3a36bd
Daniel Ribeiro Ferreira da Silva <daniel.ferreira@matriz.speedyofficer.com>
2026-02-19 11:31:41 -0300

    inicio

---
+++ src/main/java/com/daniwl/encurl/EncurlApplication.java
@@ -0,0 +1,13 @@
+package com.daniwl.encurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniwl/encurl/EncurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniwl.encurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
