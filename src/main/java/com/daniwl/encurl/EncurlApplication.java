7de2d3e735d2d1d8b922cda2bebd6c7f720c6aca
daniwl <daniel.ribeiroq@gmail.com>
2026-03-02 17:45:30 -0300

    chore: adiciona dependências e habilita suporte a cache

--- src/main/java/com/daniwl/encurl/EncurlApplication.java
+++ src/main/java/com/daniwl/encurl/EncurlApplication.java
@@ -2,8 +2,10 @@ package com.daniwl.encurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EncurlApplication {
 
 	public static void main(String[] args) {
